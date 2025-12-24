4a035ea202b99912eb7e7f3932c1b28bde69be07
Allan Ramogo <amramogo@gmail.com>
2025-12-24 23:58:28 +0300

    Modernize packages and Homepage

--- app/components/nav.tsx
+++ app/components/nav.tsx
@@ -20,14 +20,18 @@ export default function Nav() {
         <NavigationMenu>
             <NavigationMenuList className="flex space-x-6">
                 <NavigationMenuItem>
-                    <Link href="/" legacyBehavior passHref>
+                    <Link href="/">
+                        {/* @next-codemod-error This Link previously used the now removed `legacyBehavior` prop, and has a child that might not be an anchor. The codemod bailed out of lifting the child props to the Link. Check that the child component does not render an anchor, and potentially move the props manually to Link. */
+                        }
                         <NavigationMenuLink className="group inline-flex h-9 w-max items-center justify-center rounded-md bg-transparent px-4 py-2 text-sm font-medium transition-colors hover:text-amber-400 focus:text-amber-400 focus:outline-none disabled:pointer-events-none disabled:opacity-50 text-white data-[active]:bg-accent/50 data-[state=open]:bg-accent/50">
                             Home
                         </NavigationMenuLink>
                     </Link>
                 </NavigationMenuItem>
                 <NavigationMenuItem>
-                    <Link href="/about" legacyBehavior passHref>
+                    <Link href="/about">
+                        {/* @next-codemod-error This Link previously used the now removed `legacyBehavior` prop, and has a child that might not be an anchor. The codemod bailed out of lifting the child props to the Link. Check that the child component does not render an anchor, and potentially move the props manually to Link. */
+                        }
                         <NavigationMenuLink className="group inline-flex h-9 w-max items-center justify-center rounded-md bg-transparent px-4 py-2 text-sm font-medium transition-colors hover:text-amber-400 focus:text-amber-400 focus:outline-none disabled:pointer-events-none disabled:opacity-50 text-white data-[active]:bg-accent/50 data-[state=open]:bg-accent/50">
                             About Us
                         </NavigationMenuLink>
@@ -35,14 +39,18 @@ export default function Nav() {
                 </NavigationMenuItem>
 
                 <NavigationMenuItem>
-                    <Link href="/packages" legacyBehavior passHref>
+                    <Link href="/packages">
+                        {/* @next-codemod-error This Link previously used the now removed `legacyBehavior` prop, and has a child that might not be an anchor. The codemod bailed out of lifting the child props to the Link. Check that the child component does not render an anchor, and potentially move the props manually to Link. */
+                        }
                         <NavigationMenuLink className="group inline-flex h-9 w-max items-center justify-center rounded-md bg-transparent px-4 py-2 text-sm font-medium transition-colors hover:text-amber-400 focus:text-amber-400 focus:outline-none disabled:pointer-events-none disabled:opacity-50 text-white data-[active]:bg-accent/50 data-[state=open]:bg-accent/50">
                             Packages
                         </NavigationMenuLink>
                     </Link>
                 </NavigationMenuItem>
                 <NavigationMenuItem>
-                    <Link href="/gallery" legacyBehavior passHref>
+                    <Link href="/gallery">
+                        {/* @next-codemod-error This Link previously used the now removed `legacyBehavior` prop, and has a child that might not be an anchor. The codemod bailed out of lifting the child props to the Link. Check that the child component does not render an anchor, and potentially move the props manually to Link. */
+                        }
                         <NavigationMenuLink className="group inline-flex h-9 w-max items-center justify-center rounded-md bg-transparent px-4 py-2 text-sm font-medium transition-colors hover:text-amber-400 focus:text-amber-400 focus:outline-none disabled:pointer-events-none disabled:opacity-50 text-white data-[active]:bg-accent/50 data-[state=open]:bg-accent/50">
                             Gallery
                         </NavigationMenuLink>
@@ -50,6 +58,6 @@ export default function Nav() {
                 </NavigationMenuItem>
             </NavigationMenuList>
         </NavigationMenu>
-    )
+    );
 }
 
